7d47104b6de0ea74267cd50d713784121df5fc99
FruitPhD <jdavis9808@gmail.com>
2016-05-26 22:21:02 -0400

    Removed shit skybox stuff

--- src/render/Loader.java
+++ src/render/Loader.java
@@ -33,12 +33,10 @@ import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 import org.newdawn.slick.opengl.PNGDecoder;
-import org.newdawn.slick.opengl.PNGDecoder.Format;
 import org.newdawn.slick.opengl.Texture;
 import org.newdawn.slick.opengl.TextureLoader;
 
 import models.RawModel;
-import textures.TextureData;
 
 public class Loader {
 	
@@ -79,52 +77,6 @@ public class Loader {
 		int textureID = texture.getTextureID();
 		textures.add(textureID);
 		return textureID;
-	}
-	
-	
-	private TextureData decodeTextureFile(String file) {
-		int width = 0;
-		int height = 0;
-		ByteBuffer data = null;
-		try {
-			FileInputStream in = new FileInputStream(file);
-			PNGDecoder decoder = new PNGDecoder(in);
-			width = decoder.getWidth();
-			height = decoder.getHeight();
-			data = ByteBuffer.allocateDirect(4 * width * height);
-			decoder.decode(data, width * 4, PNGDecoder.RGBA);
-			data.flip();
-			in.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		return new TextureData(data, width, height);
-	}
-	
-	
-	public int loadCubeMap(List<String> faces) throws FileNotFoundException, IOException {
-		int textureId = glGenTextures();
-		glActiveTexture(GL_TEXTURE0);
-		
-		glBindTexture(GL_TEXTURE_CUBE_MAP, textureId);
-		
-		
-		for (int i = 0; i < faces.size(); i++) {
-			TextureData data = decodeTextureFile("res/" + faces.get(i) + ".png");
-			glTexImage2D(
-					GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0,
-					GL_RGBA, data.getWidth(), data.getWidth(), 0, GL_RGBA, GL_UNSIGNED_BYTE, data.getData()
-			);
-		}
-		
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-	
-		return textureId;
 	}
 	
 	public void cleanUp()
